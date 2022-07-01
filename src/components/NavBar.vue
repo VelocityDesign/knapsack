3684dcb20ee2bd21a13ad498dcbdcf1c0ccc8f3d
Judah Brown <ConvexPolyhedron@Outlook.com>
2022-07-01 02:32:55 +0000

    Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,16 +14,29 @@ export default {
 <style scoped>
   #navbar-container {
     display: flex;
-    height: 5vh;
+    height: 7.5vh;
     width: 100vw;
     background: rgb(226, 27, 27);
-    justify-content: center;
-    align-content: center;
+    align-items: center;
+    box-shadow: 0px 0px 16px -3px rgba(0, 0, 0, 0.43);
   }
   #wordmark {
     font-family: "Plus Jakarta Sans", sans-serif;
     color: white;
     font-weight: 800;
-    height: 80%; 
+    font-size: 2em; 
+  }
+  @media screen and (min-width: 700px) {
+    #navbar-container {
+        /* justify-content: center; */
+    }
+    #wordmark {
+      
+    }
+  }
+  @media screen and (max-width: 700px) {
+    #navbar-container {
+        justify-content: center;
+    }
   }
 </style>
